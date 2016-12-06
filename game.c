47975e65ebede44c61272b90ebc9b6bd4004ed5f
LaroyenneG <guillaume.laroyenne@outlook.com>
2016-12-06 17:43:59 +0100

    ok

--- game.c
+++ game.c
@@ -15,9 +15,24 @@ int main(int argc, char** argv){
     color(38);
     printf("Warning, the terminal background color must be black.\n");
     color(0);
-    party_t* p = createParty();
-    start(p);
-    free_party(p);
+    int y=1;
+    while (y){
+        printf("Do you want to play ? (yes=1/no=0)\n");
+        if(scanf("%d",&y)!=1){
+            perror("Input error, the value is not of the type integer");
+            exit(2);
+        }
+        if(y!=1){
+            y=0;
+        } else{
+            party_t* p = createParty();
+            start(p);
+            printf("Release of the memory... ");
+            free_party(p);
+            printf("done.\n");
+        }
+    }
+
     return 0;
 }
 
